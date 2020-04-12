7abcb9e58c99257d14a4015a95a38a94b437fafa
moritzvoelker <moritz.voelker@arithnea.de>
2020-04-12 15:58:27 +0200

    Added the Neat class, which is the API to use. It manages the hole evolution and evaluation process.

---
+++ src/main/java/neat/Neat.java
@@ -0,0 +1,91 @@
+package neat;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Neat {
+    private int populationSize;
+    private double speciationThreshhold;
+    private double mutationRateNode;
+    private double mutationRateConnection;
+    private List<Organism> population;
+
+    private int inputCount;
+    private int outputCount;
+
+    public Neat() {
+    }
+
+    public Neat(int populationSize, double speciationThreshhold, double mutationRateNode, double mutationRateConnection, int inputCount, int outputCount) {
+        this.populationSize = populationSize;
+        this.speciationThreshhold = speciationThreshhold;
+        this.mutationRateNode = mutationRateNode;
+        this.mutationRateConnection = mutationRateConnection;
+        this.inputCount = inputCount;
+        this.outputCount = outputCount;
+    }
+
+    public void setInput(List<double[]> input) throws IllegalArgumentException {
+    }
+
+    public List<double[]> getOutput() {
+    }
+
+    public void nextGeneration() {
+    }
+
+    private void mutate() {
+    }
+
+    public int getPopulationSize() {
+        return populationSize;
+    }
+
+    public void setPopulationSize(int populationSize) {
+        this.populationSize = populationSize;
+    }
+
+    public double getSpeciationThreshhold() {
+        return speciationThreshhold;
+    }
+
+    public void setSpeciationThreshhold(double speciationThreshhold) {
+        this.speciationThreshhold = speciationThreshhold;
+    }
+
+    public double getMutationRateNode() {
+        return mutationRateNode;
+    }
+
+    public void setMutationRateNode(double mutationRateNode) {
+        this.mutationRateNode = mutationRateNode;
+    }
+
+    public double getMutationRateConnection() {
+        return mutationRateConnection;
+    }
+
+    public void setMutationRateConnection(double mutationRateConnection) {
+        this.mutationRateConnection = mutationRateConnection;
+    }
+
+    public List<Organism> getPopulation() {
+        return population;
+    }
+
+    public int getInputCount() {
+        return inputCount;
+    }
+
+    public void setInputCount(int inputCount) {
+        this.inputCount = inputCount;
+    }
+
+    public int getOutputCount() {
+        return outputCount;
+    }
+
+    public void setOutputCount(int outputCount) {
+        this.outputCount = outputCount;
+    }
+}
