bcfdbe4a1ec8bac8e3ea38fb17b815857dfea3a1
baerking <player245@t-online.de>
2020-07-11 20:46:39 +0200

    Bugfix: the network is properly displayed and no dublicates get painted

--- src/main/java/gui/MainFrame.java
+++ src/main/java/gui/MainFrame.java
@@ -30,15 +30,21 @@ public class MainFrame extends JFrame {
         JLabel generationLabel = new JLabel("Generation " + testcase.getGeneration());
         generationLabel.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 50));
 
+        JPanel  controls = new JPanel(new GridLayout(3, 1));;
+
         JPanel champDisplay = new JPanel(new GridLayout(1, 1));
 
         JButton doGenButton = new JButton("Do one generation");
         doGenButton.addActionListener(e -> {
             testcase.doNGenerations(1);
             generationLabel.setText("Generation " + testcase.getGeneration());
+
             champDisplay.removeAll();
-            champDisplay.updateUI();
             champDisplay.add(new Display(testcase.getChamp()));
+
+
+            content.validate();;
+            content.repaint();
         });
         doGenButton.setEnabled(false);
 
@@ -47,16 +53,20 @@ public class MainFrame extends JFrame {
             testcase.init();
             doGenButton.setEnabled(true);
             generationLabel.setText("Generation " + testcase.getGeneration());
+
             champDisplay.removeAll();
-            champDisplay.updateUI();
             champDisplay.add(new Display(testcase.getChamp()));
             System.out.println("Initialized testcase");
+            content.validate();;
+            content.repaint();
         });
 
-        JPanel controls = new JPanel(new GridLayout(3, 1));
+
         controls.add(resetButton);
         controls.add(doGenButton);
         controls.add(champDisplay);
+
+
 
         content.add(generationLabel, BorderLayout.NORTH);
         content.add(controls, BorderLayout.EAST);
